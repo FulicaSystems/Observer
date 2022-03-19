6c3f830af723010f71fa4c0fd3e92315e63314b4
Jarod <j.sengkeo@student.isartdigital.com>
2022-03-19 01:34:27 +0100

    first imgui editor

--- Project_O/src/main.cpp
+++ Project_O/src/main.cpp
@@ -3,6 +3,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 #include "resources/shader.hpp"
 
 #define SCRN_WIDTH 1366
@@ -39,6 +43,10 @@ int main(void)
         return -2;
     }
 
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 450");
+
     glViewport(0, 0, SCRN_WIDTH, SCRN_HEIGHT);
 
 
@@ -61,17 +69,37 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        float fovY = 90.f;
-        float near = 0.001f;
-        float far = 1000.f;
-        vec3 camPos = { 0.f, 0.f, 1.f };
-        vec3 camRot = vec3::zo;
 
-        mat4 p = Math3::perspective(fovY, SCRN_WIDTH / (float)SCRN_HEIGHT, near, far);
+        static vec3 camPos = { 0.f, 0.f, 1.f };
+        static vec3 camRot = vec3::zo;
+        static float fovY = 90.f * TORAD;
+        static float near = 0.001f;
+        static float far = 1000.f;
+
+        ImGui_ImplGlfw_NewFrame();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Begin("Camera");
+        ImGui::DragFloat3("position", camPos.elem);
+        ImGui::DragFloat3("rotation", camRot.elem);
+        ImGui::SliderAngle("fov y", &fovY, 0.f, 180.f);
+        ImGui::DragFloat("near", &near);
+        ImGui::DragFloat("far", &far);
+        ImGui::End();
+
+        ImGui::Render();
+
+
+        mat4 p = Math3::perspective(fovY * TODEG, SCRN_WIDTH / (float)SCRN_HEIGHT, near, far);
         mat4 v = Math3::translateMatrix(-camPos) * Math3::rotateXMatrix(camRot.x) * Math3::rotateYMatrix(-camRot.y) * Math3::rotateZMatrix(camRot.z);
         mat4 m = mat4::id;
+
+        glEnable(GL_DEPTH_TEST);
 
         sh.use();
         sh.uniformMat4("uProjection", p, false);
@@ -81,6 +109,11 @@ int main(void)
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
+
+        glDisable(GL_DEPTH_TEST);
+
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 
         /* Swap front and back buffers */
