92dbd203f19c2bdacbc1e3e4a43e28456cbb8e71
SJarod <jarsengkeoPro@gmail.com>
2026-03-31 18:12:12 +0200

    swapchain created

--- src/client/application.cpp
+++ src/client/application.cpp
@@ -29,53 +29,55 @@ Application::Application()
                      "VK_LAYER_KHRONOS_validation", "VK_LAYER_LUNARG_monitor",
                      },
         .instanceExtensions = ext,
-#ifdef ENABLE_VIDEO_TRANSCODE
         .deviceExtensions =
             {
+                     VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+#ifdef ENABLE_VIDEO_TRANSCODE
                      VK_KHR_VIDEO_DECODE_H264_EXTENSION_NAME, VK_KHR_VIDEO_DECODE_H265_EXTENSION_NAME,
                      VK_KHR_VIDEO_DECODE_QUEUE_EXTENSION_NAME, VK_KHR_VIDEO_QUEUE_EXTENSION_NAME,
-                     },
 #endif
+                     },
     });
     m_instance = std::make_unique<Instance>(InstanceCreateInfoT{m_context.get()});
 
     auto surface = m_window->createSurface(m_context.get(), m_instance.get());
 
     int i = -1;
-    for (const std::string &physicalDeviceName : m_instance->enumerateAvailablePhysicalDevices(false))
+    for (const std::string& physicalDeviceName :
+         m_instance->enumerateAvailablePhysicalDevices(false))
     {
         ++i;
 
-        std::shared_ptr<PhysicalDevice> physicalDevice = std::make_shared<PhysicalDevice>(PhysicalDeviceCreateInfoT{
-            .cx = m_context.get(),
-            .inst = m_instance.get(),
-            .deviceName = physicalDeviceName.c_str(),
-            .surface = surface,
-        });
+        std::shared_ptr<PhysicalDevice> physicalDevice =
+            std::make_shared<PhysicalDevice>(PhysicalDeviceCreateInfoT{
+                .cx = m_context.get(),
+                .inst = m_instance.get(),
+                .deviceName = physicalDeviceName.c_str(),
+                .surface = surface,
+            });
         assert(physicalDevice);
 
         m_physicalDevices.push_back(physicalDevice);
         m_devices.emplace_back(physicalDevice->createDevice());
 
-        if (m_currentDeviceIndex < 0 && physicalDevice->getDeviceType() == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+        if (m_currentDeviceIndex < 0 &&
+            physicalDevice->getDeviceType() == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
             m_currentDeviceIndex = i;
     }
-    if (i < 0)
-        i = 0;
 
     m_window->setSwapChain(m_devices[m_currentDeviceIndex]->createSwapChain(SwapChainCreateInfoT{
         .surface = surface,
         .surfaceFormat =
             {
-                .format = VK_FORMAT_B8G8R8A8_UNORM,
-                .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR,
-            },
+                            .format = VK_FORMAT_B8G8R8A8_UNORM,
+                            .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR,
+                            },
         .presentMode = VK_PRESENT_MODE_FIFO_KHR,
         .extent =
             {
-                .width = width,
-                .height = height,
-            },
+                            .width = width,
+                            .height = height,
+                            },
     }));
 }
 
